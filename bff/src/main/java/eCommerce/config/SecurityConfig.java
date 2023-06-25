a37ba2e22c490b3ad53dbc33e7b82652ee7bdd6d
Julius <julius.thomsen99@gmail.com>
2023-06-25 20:13:49 +0200

    Add config for authorization with Google

--- bff/src/main/java/eCommerce/config/SecurityConfig.java
+++ bff/src/main/java/eCommerce/config/SecurityConfig.java
@@ -1,14 +1,23 @@
 package eCommerce.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        return http.build();
+        return http
+                .csrf().disable()
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login().and()
+                .build();
     }
 
 }
