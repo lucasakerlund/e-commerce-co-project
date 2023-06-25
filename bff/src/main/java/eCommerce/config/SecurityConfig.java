ea96caef9b178b3b9ae7c58e02812971d188f73d
lucasakerlund <lucas2001@hotmail.se>
2023-06-25 19:30:46 +0200

    Add functionality to load frontend on spring boot start

---
+++ bff/src/main/java/eCommerce/config/SecurityConfig.java
@@ -0,0 +1,14 @@
+package eCommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
+}
